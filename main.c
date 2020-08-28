1bdbba813b2913dccd6caa408103cf44754dd956
Chris Behan <chrisbehan1997@gmail.com>
2020-08-28 07:12:22 -0500

    Adds snake motion for a snake of length 2.

--- main.c
+++ main.c
@@ -15,14 +15,17 @@ typedef enum
   RIGHT
 } direction;
 
-typedef struct
+typedef struct snake
 {
   int x;
   int y;
+  int prev_x;
+  int prev_y;
   int size;
   int width;
   int height;
   direction dir;
+  struct snake *tail;
 } snake;
 
 typedef struct
@@ -46,7 +49,7 @@ int fruit_collision(snake *, fruit *);
 void handle_collision(snake *, fruit *);
 int get_random_x(void);
 int get_random_y(void);
-
+void grow_snake(snake *);
 int main()
 {
   // Initialization
@@ -54,7 +57,7 @@ int main()
 
   InitWindow(SCREENWIDTH, SCREENHEIGHT, "snake");
 
-  snake player = {TILES / 2 * TILE_WIDTH, TILES / 2 * TILE_WIDTH, 1, TILE_WIDTH, TILE_WIDTH, RIGHT};
+  snake player = {TILES / 2 * TILE_WIDTH, TILES / 2 * TILE_WIDTH, 1, 0, 0, TILE_WIDTH, TILE_WIDTH, RIGHT, NULL};
   fruit fruit = {get_random_x(), get_random_y()};
   SetTargetFPS(60); // Set our game to run at 60 frames-per-second
   //--------------------------------------------------------------------------------------
@@ -72,7 +75,7 @@ int main()
     BeginDrawing();
     if (game_over)
     {
-      //ClearBackground(RAYWHITE);
+      ClearBackground(RAYWHITE);
       DrawText("GAME OVER", SCREENWIDTH / 2 - 70, SCREENHEIGHT / 2 - 100, 24, BLACK);
     }
     else
@@ -110,12 +113,18 @@ void draw_snake(snake *s)
 {
   set_direction(s);
   move_snake(s);
-  //if (wall_collision(s))
-  //{
-  //  game_over = 1;
-  //  return;
-  //}
   DrawRectangle(s->x, s->y, s->width, s->height, MAGENTA);
+  while (s->tail != NULL)
+  {
+    int new_x = s->prev_x;
+    int new_y = s->prev_y;
+    s = s->tail;
+    s->prev_x = s->x;
+    s->prev_y = s->y;
+    s->x = new_x;
+    s->y = new_y;
+    DrawRectangle(s->x, s->y, s->width, s->height, MAGENTA);
+  }
 }
 
 void move_snake(snake *s)
@@ -125,16 +134,24 @@ void move_snake(snake *s)
     switch (s->dir)
     {
     case UP:
+      s->prev_y = s->y;
       s->y -= TILE_WIDTH;
+      s->prev_x = s->x;
       break;
     case DOWN:
+      s->prev_y = s->y;
       s->y += TILE_WIDTH;
+      s->prev_x = s->x;
       break;
     case LEFT:
+      s->prev_x = s->x;
       s->x -= TILE_WIDTH;
+      s->prev_y = s->y;
       break;
     case RIGHT:
+      s->prev_x = s->x;
       s->x += TILE_WIDTH;
+      s->prev_y = s->y;
       break;
     default:
       break;
@@ -214,6 +231,7 @@ void handle_collision(snake *s, fruit *f)
   {
     s->size += 1;
     f->eaten = 1;
+    grow_snake(s);
   }
   else
   {
@@ -229,4 +247,24 @@ int get_random_x(void)
 int get_random_y(void)
 {
   return rand() % TILES * TILE_WIDTH;
+}
+
+void grow_snake(snake *s)
+{
+  // loop through every part of the snake until we are at its tail
+  while (s->tail != NULL)
+  {
+    s = s->tail;
+  }
+  struct snake *new_tail = (struct snake *)malloc(sizeof(*new_tail));
+  s->tail = new_tail;
+  new_tail->x = s->x;
+  new_tail->y = s->y;
+  new_tail->prev_x = s->prev_x;
+  new_tail->prev_y = s->prev_y;
+  new_tail->width = s->width;
+  new_tail->height = s->height;
+  new_tail->size = s->size;
+  new_tail->dir = s->dir;
+  new_tail->tail = NULL;
 }
